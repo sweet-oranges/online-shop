d9505d4d004785fec4c51216f8e8edab3cd8bd39
sweet-orange <xl.zeng@outlook.com>
2019-07-09 16:54:35 +0800

    7-9 完成登录页面的背景效果布局

---
+++ src/components/login/login.vue
@@ -0,0 +1,80 @@
+
+<style lang="less" >
+    @import "login.less";
+</style>
+
+<template>
+   <div class="login">
+       <vue-particles
+        color="#F4F8F8"
+        :particleOpacity="0.7"
+        :particlesNumber="30"
+        shapeType="star"
+        :particleSize="4"
+        linesColor="#ccc"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="2"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+        class="vfx"
+      >
+      </vue-particles>
+
+      <div class="container" >
+      <div class="login-title">
+        <p class="title-above test">Orange V-1.0</p>
+        <p class="title-below test">欢迎使用橘子办公系统</p>
+      </div>
+    </div>
+
+    <div class="login-con">
+      <!-- <Card icon="log-in" title="欢迎登录" :bordered="false">
+        <div class="form-con">
+          <login-form @on-success-valid="handleSubmit"></login-form>
+          <p class="login-tip">输入用户名密码进行登录</p>
+        </div>
+      </Card> -->
+        
+
+    </div>
+   </div>
+</template>
+
+<script>
+import LoginForm from '../login/login-form'
+import { mapActions } from 'vuex'
+export default{
+    components: {
+        LoginForm
+    },
+    methods: {
+    ...mapActions([
+      'handleLogin',
+      'getUserInfo'
+    ]),
+    handleSubmit ({ userName, password }) {
+      this.handleLogin({ userName, password }).then(res => {
+        console.log(res)
+        if (res.token === '') {
+          console.log('暂无此账号')
+          this.$Message.error('用户名或者密码输入错误')
+        } else {
+          this.getUserInfo().then(res => {
+            this.$router.push({
+              name: this.$config.homeName
+            })
+          })
+        }
+      })
+    }
+  }
+
+}
+</script>
+
+
--- src/main.js
+++ src/main.js
@@ -3,9 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex'
 import $ from 'jquery'
+import 'bootstrap'
+import VueParticles from 'vue-particles' 
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
 Vue.config.productionTip = false
+Vue.use(VueParticles) 
+Vue.use(Vuex)
+Vue.use(iView)
 
 /* eslint-disable no-new */
 new Vue({
